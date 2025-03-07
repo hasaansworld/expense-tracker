bac5d2909f1afdcf085a4df5aca5f95afb4ba4cc
Hasaan Ahmed <hasaanahmed.pk@gmail.com>
2025-03-07 06:20:53 +0200

    removed app.py file

--- expenses/__init__.py
+++ expenses/__init__.py
@@ -5,7 +5,8 @@ from flask_caching import Cache
 from models import init_db_command
 from api import api_bp
 from expenses.utils import UserConverter, GroupConverter, ExpenseConverter
-    
+from werkzeug.exceptions import NotFound, Conflict, BadRequest, UnsupportedMediaType, Forbidden
+
 
 db = SQLAlchemy()
 cache = Cache()
@@ -41,5 +42,27 @@ def create_app(test_config=None):
     app.url_map.converters['expense'] = ExpenseConverter
 
     app.register_blueprint(api_bp)
+
+    # Return errors in json
+    def handle_not_found(error):
+        return {"message": str(error)}, 404
+
+    def handle_bad_request(error):
+        return {"message": str(error)}, 400
+
+    def handle_unsupported_media_type(error):
+        return {"message": str(error)}, 415
+
+    def handle_conflict(error):
+        return {"message": str(error)}, 409
+
+    def handle_forbidden(error):
+        return {"message": str(error)}, 403
+
+    api_bp.errorhandler(NotFound)(handle_not_found)
+    api_bp.errorhandler(BadRequest)(handle_bad_request)
+    api_bp.errorhandler(UnsupportedMediaType)(handle_unsupported_media_type)
+    api_bp.errorhandler(Conflict)(handle_conflict)
+    api_bp.errorhandler(Forbidden)(handle_forbidden)
 
     return app
